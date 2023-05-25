cab5370b6ac0a7dc643078e99549a42686707af2
Jacob-zac <jacobrz030@gmail.com>
2023-05-25 12:49:47 -0700

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$post = $('#post'),
+		$sidebar = $('#sidebar'),
+		$main = $('#main');
+		$main1 = $('#main1');
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ null,      '480px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+	// Intro.
+		var $intro = $('#intro');
+
+		// Move to main on <=large, back to sidebar on >large.
+			breakpoints.on('<=large', function() {
+				$intro.prependTo($main);
+			});
+
+			breakpoints.on('>large', function() {
+				$intro.prependTo($sidebar);
+			});
+
+})(jQuery);
